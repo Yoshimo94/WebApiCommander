2200f05e538ee1d61fe7ef79764d6050cdad32a1
Yoshimo94 <maciekprzygoda@interia.pl>
2022-03-13 19:13:06 +0100

    Initialize project with first Command models and repo mock

---
+++ WebApiCommander/WebApiCommander/Data/ICommanderRepo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using WebApiCommander.Models;
+
+namespace WebApiCommander.Data
+{
+    public interface ICommanderRepo
+    {
+        IEnumerable<Command> GetAppCommands();
+        Command GetCommandById(int id);
+    }
+}
