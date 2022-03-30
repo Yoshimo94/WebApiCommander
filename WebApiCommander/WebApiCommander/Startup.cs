47d4f315e0b37d759a894fe5cc460e28f7c4fc05
Yoshimo94 <maciekprzygoda@interia.pl>
2022-03-30 20:04:33 +0200

    Add migration folder and add commander to DB

--- WebApiCommander/WebApiCommander/Startup.cs
+++ WebApiCommander/WebApiCommander/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,6 +23,9 @@ namespace WebApiCommander
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommanderContext>(opt => opt.UseSqlServer
+            (Configuration.GetConnectionString("CommanderConnection")));
+
             services.AddControllers();
 
             services.AddScoped<ICommanderRepo, MockCommanderRepo>();
