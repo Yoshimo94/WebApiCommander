0d73c7bda78c73966ceddae9990149c72c6f3a3d
Yoshimo94 <maciekprzygoda@interia.pl>
2022-04-03 18:15:52 +0200

    Add SaveChanges and CreateCommand method

--- WebApiCommander/WebApiCommander/Data/SqlCommanderRepo.cs
+++ WebApiCommander/WebApiCommander/Data/SqlCommanderRepo.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using WebApiCommander.Models;
 
@@ -12,6 +13,21 @@ namespace WebApiCommander.Data
         {
             _context = context;
         }
+
+        public void CreateCommand(Command cmd)
+        {
+            if(cmd == null)
+            {
+                throw new ArgumentNullException(nameof(cmd));
+            }
+            _context.Commands.Add(cmd);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+
         IEnumerable<Command> ICommanderRepo.GetAllCommands()
         {
             return _context.Commands.ToList();
